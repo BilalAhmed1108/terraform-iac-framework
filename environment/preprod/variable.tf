916cb99df03fa1960b30b68645495f564193ec15
BilalAhmed1108 <your.email@example.com>
2025-11-11 20:15:12 +0530

    till sqldatabase, vm next

--- environment/preprod/variable.tf
+++ environment/preprod/variable.tf
@@ -250,3 +250,90 @@ variable "kvsec" {
     kvid                           = string
   }))
 }
+
+variable "sqlserver" {
+    type =map(object({
+        name                         = string
+        resource_group_name          = string
+        location                     = string
+        version                      = string
+        sql_password                  = string
+        sql_username                  = string
+        minimum_tls_version          = optional(string)
+        administrator_login_password_wo = optional(string)
+        administrator_login_password_wo_version = optional(string)
+        connection_policy            = optional(string)
+        express_vulnerability_assessment_enabled = optional(bool)
+        transparent_data_encryption_key_vault_key_id = optional(string)
+        public_network_access_enabled = optional(bool)
+        outbound_traffic_enabled     = optional(bool)
+        primary_user_assigned_identity_id = optional(string)
+        identity = optional(object({
+            type = string
+            identity_ids = optional(list(string))
+        }))
+        azuread_administrator = optional(object({
+            login_username = string
+            object_id      = string
+            tenant_id      = optional(string)
+            azuread_authentication_only = optional(bool)
+        }))
+        tags = optional(map(string))
+    }))
+  
+}
+
+variable "keyvaultid" {
+    type = map(object({
+        r_g_n = string
+        name  = string
+    }))
+  
+}
+
+
+# SQLServer variable
+variable "database" {
+  type = map(object({
+    name= optional(string)
+    auto_pause_delay_in_minutes = optional(string)
+    collation    = optional(string)
+  license_type = optional(string)
+  max_size_gb  = optional(string)
+  sku_name     = optional(string)
+  enclave_type=   optional(string)
+create_mode= optional(string)  
+creation_source_database_id = optional(string)   
+elastic_pool_id = optional(string)  
+geo_backup_enabled= optional(string)
+maintenance_configuration_name = optional(string)
+ledger_enabled = optional(bool)  
+ min_capacity = optional(string)
+ restore_point_in_time = optional(string)
+ recover_database_id = optional(string)
+ recovery_point_id = optional(string)
+ restore_dropped_database_id=   optional(string)
+ restore_long_term_retention_backup_id = optional(string)
+ read_replica_count = optional(string)
+ read_scale= optional(string)
+sample_name = optional(string)
+storage_account_type = optional(string)
+ transparent_data_encryption_enabled = optional(bool)
+ transparent_data_encryption_key_vault_key_id= optional(string)
+ transparent_data_encryption_key_automatic_rotation_enabled=optional(bool)
+ zone_redundant = optional(bool)
+ secondary_type =optional(string)
+
+  tags = optional(map(string))
+  }))
+}
+
+variable "sqlserverdata" {
+  type = map(object({
+    name = string
+    r_g_n=string
+  }))
+}
+
+
+
