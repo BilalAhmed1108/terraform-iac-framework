d81aae95a10b039d689fa2e16783521f4ad626cf
BilalAhmed1108 <your.email@example.com>
2025-11-24 20:08:34 +0530

    akr

--- environment/preprod/provider.tf
+++ environment/preprod/provider.tf
@@ -16,7 +16,7 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "****"
+  subscription_id = "*****"
   client_id       = "****"
   client_secret   = "****"
   tenant_id       = "****"
